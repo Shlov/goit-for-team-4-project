5ccd3164ea9ece232b87820df830cb640f59c81f
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-16 18:06:45 +0200

    Add langArr, new classes
    Add language array (en & ua words)
    Add new classes "lang" to switch language in "header-home" section

---
+++ src/js/langArr.js
@@ -0,0 +1,19 @@
+const langArray = {
+  headerTitle: {
+    ua: 'Фільмотека',
+    en: 'Filmoteka',
+  },
+  homepage: {
+    ua: 'Домашня сторінка',
+    en: 'Home',
+  },
+  myLibrary: {
+    ua: 'Моя бібліотека',
+    en: 'My library',
+  },
+  movieSearch: {
+    ua: 'Пошук фильмів',
+    en: 'Movie search',
+  },
+  
+};
