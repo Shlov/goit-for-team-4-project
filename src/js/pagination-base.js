42137384dac6c32124e2c4c4b01569ec4e47c419
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-12-17 22:43:47 +0200

    Pagination-2 started

--- src/js/pagination-base.js
+++ src/js/pagination-base.js
@@ -1,6 +1,9 @@
 // Load inner sources
 import createTypicalPaginationNavigation from "./pagination-create-typical-nav";
 import createMobilePaginationNavigation from "./pagination-create-mobile-nav";
+import NewsApiService from "./fetch";
+const Fetch = new NewsApiService;
+Fetch.fetchPopularMovie().then(data => console.log('My-data', data.total_pages, data.page));
 
 const paginationClickForInput = document.querySelector(".pagination-click");
 const paginationInput = document.querySelector(".pagination-input");
