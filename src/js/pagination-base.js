800726ccd5ee93de246f64d7f5c9bbf898828ffb
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-12-17 23:47:51 +0200

    Events listener for buttons added but need modification

--- src/js/pagination-base.js
+++ src/js/pagination-base.js
@@ -11,11 +11,12 @@ const paginationInput = document.querySelector(".pagination-input");
 const paginationRef = document.querySelector('.js-pagination');
 
 const data = {
-    page: 10,
-    totalPages: 25,
+    page: 1,
+    totalPages: 8,
 }
 
 function changeViewportWhenStart(data) {
+    paginationRef.innerHTML = '';
     if (window.innerWidth >= 480) {        
         createTypicalPaginationNavigation(data);
     } else {
@@ -32,5 +33,18 @@ function onChangeWindow(evt) {
         createTypicalPaginationNavigation(data);
     } else {        
         createMobilePaginationNavigation(data);
+    }
+}
+
+paginationRef.addEventListener('click', onPaginationNavigationClick);
+function onPaginationNavigationClick(evt) {
+    const { tagName, name, textContent } = evt.target;
+    console.log(name);
+    if (tagName !== 'BUTTON') {
+        return;
+    } else {
+        if ((name === 'number-page') || (name === 'first-page') || (name === 'last-page')) {
+            
+        }
     }
 }
--- src/js/pagination-create-mobile-nav.js
+++ src/js/pagination-create-mobile-nav.js
@@ -87,9 +87,9 @@ function selectConditionOfButton(i, page) {
             let text = '';
             
             if (i === page) {
-                text = `<button type="button" class="js-pagination__btn js-pagination__btn--current" name="number-page" disabled><span>${i}</span></button>`
+                text = `<button type="button" class="js-pagination__btn js-pagination__btn--current" name="number-page" disabled>${i}</button>`
                 } else {
-                text = `<button type="button" class="js-pagination__btn js-pagination__btn--active" name="number-page"><span>${i}</span></button>`
+                text = `<button type="button" class="js-pagination__btn js-pagination__btn--active" name="number-page">${i}</button>`
             }
             return text
         }
