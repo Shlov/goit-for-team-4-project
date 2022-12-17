b0fd624f89836f7b77dc48ccfafd6e9eb3e30315
katya20220910 <katyaantonova009@gmail.com>
2022-12-17 23:09:09 +0200

    cards

--- src/js/render-top-films.js
+++ src/js/render-top-films.js
@@ -31,10 +31,10 @@ function renderGalleryFilms(cards) {
       vote_average,
     } = card;
     return  `<li class="card card-js" data-id="${id}">
-    <a class="card__link" href="#">
+    <a class="card__link lnk" href="#">
       <img class="card__img" src="${poster_path}" alt="${original_title}" width="395" height="574">
-      <h3 class="card__name card__name-lang">${original_title}Test Name</h3>
-      <p class="card__info card__info-lang"> ${genre_ids} Test Drama | Test 2000 ${release_date}<span class="card__rating">  Test Rating ${vote_average} 10.0</span></p>
+      <h3 class="card__name card__name-lang">${original_title}</h3>
+      <p class="card__info card__info-lang"> ${genre_ids} | ${release_date}<span class="card__rating"> ${vote_average} </span></p>
     </a>
   </li>` }).join('')
   refs.card.insertAdjacentHTML('beforeend', markup);
