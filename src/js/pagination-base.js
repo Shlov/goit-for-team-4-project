15b93f73a91aa9b494536dd8b1bfe410f7acdc11
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-12-18 17:51:04 +0200

    final for pagination panel functionality

--- src/js/pagination-base.js
+++ src/js/pagination-base.js
@@ -1,50 +1,100 @@
 // Load inner sources
-import createTypicalPaginationNavigation from "./pagination-create-typical-nav";
-import createMobilePaginationNavigation from "./pagination-create-mobile-nav";
-import NewsApiService from "./fetch";
-const Fetch = new NewsApiService;
-Fetch.fetchPopularMovie().then(data => console.log('My-data', data.total_pages, data.page));
+import createTypicalPaginationNavigation from './pagination-create-typical-nav';
+import createMobilePaginationNavigation from './pagination-create-mobile-nav';
+import NewsApiService from './fetch';
 
-const paginationClickForInput = document.querySelector(".pagination-click");
-const paginationInput = document.querySelector(".pagination-input");
+// Create starting pagination panel
+const startFetch = new NewsApiService();
+startFetch.fetchPopularMovie().then(data => changeViewportAndData(data));
 
+// Link on HTML / DOM elements
 const paginationRef = document.querySelector('.js-pagination');
 
-const data = {
-    page: 1,
-    totalPages: 8,
+// Support pagination data
+let paginationData = {
+  page: 0,
+  totalPages: 0,
+};
+
+// export function switchPagination(num) {
+//     if (num === 0) {
+//         startFetch.fetchPopularMovie().then(data => console.log('My-data', data.total_pages, data.page));
+//     } else if (num === 1) {
+//         console.log("запустилась 1 версія");
+//     }
+// }
+
+// Here starting panel pagination
+function changeViewportAndData(data) {
+  let newData;
+  if (!data.total_results) {
+    return;
+  } else if (data.total_pages > 500) {
+    newData = {
+      page: data.page,
+      totalPages: 500,
+    };
+  } else {
+    newData = {
+      page: data.page,
+      totalPages: data.total_pages,
+    };
+  }
+
+  paginationData = newData;
+
+  paginationRef.innerHTML = '';
+  if (window.innerWidth >= 480) {
+    createTypicalPaginationNavigation(newData); // Viewport width for mobile device
+  } else {
+    createMobilePaginationNavigation(newData); // Viewport width for tab and desktop device
+  }
 }
 
-function changeViewportWhenStart(data) {
-    paginationRef.innerHTML = '';
-    if (window.innerWidth >= 480) {        
-        createTypicalPaginationNavigation(data);
-    } else {
-        createMobilePaginationNavigation(data);
-    }    
-}
-
-changeViewportWhenStart(data);
-
-window.addEventListener("resize", onChangeWindow);
+// Change form on resize viewport
+window.addEventListener('resize', onChangeWindow);
 function onChangeWindow(evt) {
-    paginationRef.innerHTML = '';    
-    if (evt.target.innerWidth >= 480) {        
-        createTypicalPaginationNavigation(data);
-    } else {        
-        createMobilePaginationNavigation(data);
-    }
+  paginationRef.innerHTML = '';
+  if (evt.target.innerWidth >= 480) {
+    createTypicalPaginationNavigation(paginationData);
+  } else {
+    createMobilePaginationNavigation(paginationData);
+  }
 }
 
+// Behavior pagination panel and render image(s) on click
 paginationRef.addEventListener('click', onPaginationNavigationClick);
 function onPaginationNavigationClick(evt) {
-    const { tagName, name, textContent } = evt.target;
-    console.log(name);
-    if (tagName !== 'BUTTON') {
-        return;
-    } else {
-        if ((name === 'number-page') || (name === 'first-page') || (name === 'last-page')) {
-            
-        }
+  const { tagName, name, textContent } = evt.target;
+
+  if (tagName !== 'BUTTON') {
+    return;
+  } else {
+    if (
+      name === 'number-page' ||
+      name === 'first-page' ||
+      name === 'last-page'
+    ) {
+      startFetch.page = textContent;
+      paginationData.page = textContent;
+      startFetch.fetchPopularMovie().then(data => {
+          changeViewportAndData(data);
+        //   renderGalleryFilms(data.results);
+      });
+    } else if (name === 'pre-page') {
+      paginationData.page -= 1;
+      startFetch.page = paginationData.page;
+        startFetch.fetchPopularMovie().then(data => {
+            changeViewportAndData(data);
+            //   renderGalleryFilms(data.results);
+        });
+    } else if (name === 'next-page') {
+      paginationData.page += 1;
+      startFetch.page = paginationData.page;            
+      startFetch.fetchPopularMovie().then(data => {
+                changeViewportAndData(data);
+                //   renderGalleryFilms(data.results);
+            });
     }
+  }
 }
