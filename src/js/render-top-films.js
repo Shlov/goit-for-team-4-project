b906c32b680c22311d2ec6607e6c91c16fe3c221
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-19 16:43:08 +0200

    Сделал читабельную шаблонную строку

--- src/js/render-top-films.js
+++ src/js/render-top-films.js
@@ -28,15 +28,28 @@ function renderGalleryFilms(cards) {
         title,
         vote_average,
       } = card;
-      return `<li class="card card-js" data-id="${id}"> <div>
-    <button data-id="${id}" class="button-youtube"></button>
-  </div><div class="card__wrap">
-      <img class="card__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" width="395" height="574">
-      <h3 class="card__name lng-cardName">${original_title}</h3>
-      <p class="card__info lng-cardInfo"> ${genre_ids} | ${
+      return `<li class="card card-js" data-id="${id}">
+      <div>
+        <button data-id="${id}" class="button-youtube"></button>
+      </div>
+      <div class="card__wrap">
+        <img
+          class="card__img"
+          src="https://image.tmdb.org/t/p/w500${poster_path}"
+          alt="${original_title}"
+          width="395"
+          height="574"
+        />
+        <h3 class="card__name lng-cardName">${original_title}</h3>
+        <p class="card__info lng-cardInfo">
+          ${genre_ids} | ${
         release_date.split('-')[0]
-      }<span class="card__rating"> ${vote_average} </span></p></div>
-  </li>`;
+      }<span class="card__rating">
+            ${vote_average}
+          </span>
+        </p>
+      </div>
+    </li>`;
     })
     .join('');
   refs.card.insertAdjacentHTML('beforeend', markup);
