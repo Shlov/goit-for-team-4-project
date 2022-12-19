94dacf1d2afdee57de8a33b3047dd894644becba
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2022-12-19 13:20:08 +0200

    new6

--- src/js/for-teams.js
+++ src/js/for-teams.js
@@ -1,186 +1,186 @@
 
-// import * as basicLightbox from 'basiclightbox';
+// // import * as basicLightbox from 'basiclightbox';
 
-import andreyUrl from '../images/footer/teem-1.jpg';
-import antonUrl from '../images/footer/teem-2.jpg';
-import antoninaUrl from '../images/footer/teem-3.jpg';
 // import andreyUrl from '../images/footer/teem-1.jpg';
-// import andreyUrl from '../images/footer/teem-1.jpg';
-// import andreyUrl from '../images/footer/teem-1.jpg';
-// import andreyUrl from '../images/footer/teem-1.jpg';
-// import andreyUrl from '../images/footer/teem-1.jpg';
-// import andreyUrl from '../images/footer/teem-1.jpg';
+// import antonUrl from '../images/footer/teem-2.jpg';
+// import antoninaUrl from '../images/footer/teem-3.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
+// // import andreyUrl from '../images/footer/teem-1.jpg';
 
-import spriteUrl from '../images/symbol.svg';
+// import spriteUrl from '../images/symbol.svg';
 
-const developers = [
-    {
-        nameDev: 'Anton',
-        surnameDev: 'Shabalov',
-        photo: `${antonUrl}`,
-        roleDev: 'Team lead',
-        gitDev: 'https://github.com/Shlov',
-    },
-    // 2
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 3
-    {
-        nameDev: 'Antonina',
-        surnameDev: 'Buchak',
-        photo: `${antoninaUrl}`,
-        roleDev: 'Frontend',
-        gitDev: 'https://github.com/AninotA',
-    },
-    // 4
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 5
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 6
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 7
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 8
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // 9
-    {
-        nameDev: 'Andrey',
-        surnameDev: 'Smit',
-        photo: `${andreyUrl}`,
-        roleDev: 'Scrum master',
-        gitDev: 'https://github.com/KuznecovAndrey7777777',
-    },
-    // // 10
-    // {
-    //     nameDev: 'Andrey',
-    //     surnameDev: 'Smit',
-    //     photo: `${andreyUrl}`,
-    //     roleDev: 'Scrum master',
-    //     gitDev: 'https://github.com/KuznecovAndrey7777777',
-    // },
-    // // 11
-    // {
-    //     nameDev: 'Andrey',
-    //     surnameDev: 'Smit',
-    //     photo: `${andreyUrl}`,
-    //     roleDev: 'Scrum master',
-    //     gitDev: 'https://github.com/KuznecovAndrey7777777',
-    // },
-    // // 12
-    // {
-    //     nameDev: 'Andrey',
-    //     surnameDev: 'Smit',
-    //     photo: `${andreyUrl}`,
-    //     roleDev: 'Scrum master',
-    //     gitDev: 'https://github.com/KuznecovAndrey7777777',
-    // },
-    // // 13
-    // {
-    //     nameDev: 'Andrey',
-    //     surnameDev: 'Smit',
-    //     photo: `${andreyUrl}`,
-    //     roleDev: 'Scrum master',
-    //     gitDev: 'https://github.com/KuznecovAndrey7777777',
-    // },
-    // // 14
-    // {
-    //     nameDev: 'Andrey',
-    //     surnameDev: 'Smit',
-    //     photo: `${andreyUrl}`,
-    //     roleDev: 'Scrum master',
-    //     gitDev: 'https://github.com/KuznecovAndrey7777777',
-    // },
-];
+// const developers = [
+//     {
+//         nameDev: 'Anton',
+//         surnameDev: 'Shabalov',
+//         photo: `${antonUrl}`,
+//         roleDev: 'Team lead',
+//         gitDev: 'https://github.com/Shlov',
+//     },
+//     // 2
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 3
+//     {
+//         nameDev: 'Antonina',
+//         surnameDev: 'Buchak',
+//         photo: `${antoninaUrl}`,
+//         roleDev: 'Frontend',
+//         gitDev: 'https://github.com/AninotA',
+//     },
+//     // 4
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 5
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 6
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 7
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 8
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // 9
+//     {
+//         nameDev: 'Andrey',
+//         surnameDev: 'Smit',
+//         photo: `${andreyUrl}`,
+//         roleDev: 'Scrum master',
+//         gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     },
+//     // // 10
+//     // {
+//     //     nameDev: 'Andrey',
+//     //     surnameDev: 'Smit',
+//     //     photo: `${andreyUrl}`,
+//     //     roleDev: 'Scrum master',
+//     //     gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     // },
+//     // // 11
+//     // {
+//     //     nameDev: 'Andrey',
+//     //     surnameDev: 'Smit',
+//     //     photo: `${andreyUrl}`,
+//     //     roleDev: 'Scrum master',
+//     //     gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     // },
+//     // // 12
+//     // {
+//     //     nameDev: 'Andrey',
+//     //     surnameDev: 'Smit',
+//     //     photo: `${andreyUrl}`,
+//     //     roleDev: 'Scrum master',
+//     //     gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     // },
+//     // // 13
+//     // {
+//     //     nameDev: 'Andrey',
+//     //     surnameDev: 'Smit',
+//     //     photo: `${andreyUrl}`,
+//     //     roleDev: 'Scrum master',
+//     //     gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     // },
+//     // // 14
+//     // {
+//     //     nameDev: 'Andrey',
+//     //     surnameDev: 'Smit',
+//     //     photo: `${andreyUrl}`,
+//     //     roleDev: 'Scrum master',
+//     //     gitDev: 'https://github.com/KuznecovAndrey7777777',
+//     // },
+// ];
 
-const markupTeamCard = developers
-    .map(({ surnameDev, nameDev, photo, roleDev, gitDev }) => {
-        return `
-<li class="team-card">
-    <img src="${photo}" alt="${nameDev}" class="team-image">
-    <p class="team-name">${surnameDev}<br>${nameDev}</p>
-    <p class="team-role">${roleDev}</p>
-    <a href="${gitDev}" target="_blank" class="team-git"><svg width="24" height="24">
-        <use href="${spriteUrl}#icon-github"></use>
-    </svg></a>
-</li>`;
-    })
-    .join('');
-const markupModal = `<p class="team-title">FourTeam<span class="logogoit"></span></p>
-<ul class="team-wrapper">
-${markupTeamCard}
-</ul>
-</div>`;
-const container = document.querySelector('.developers');
+// const markupTeamCard = developers
+//     .map(({ surnameDev, nameDev, photo, roleDev, gitDev }) => {
+//         return `
+// <li class="team-card">
+//     <img src="${photo}" alt="${nameDev}" class="team-image">
+//     <p class="team-name">${surnameDev}<br>${nameDev}</p>
+//     <p class="team-role">${roleDev}</p>
+//     <a href="${gitDev}" target="_blank" class="team-git"><svg width="24" height="24">
+//         <use href="${spriteUrl}#icon-github"></use>
+//     </svg></a>
+// </li>`;
+//     })
+//     .join('');
+// const markupModal = `<p class="team-title">FourTeam<span class="logogoit"></span></p>
+// <ul class="team-wrapper">
+// ${markupTeamCard}
+// </ul>
+// </div>`;
+// const container = document.querySelector('.developers');
 
-container.addEventListener('click', openModal);
+// container.addEventListener('click', openModal);
 
-const modal = basicLightbox.create(markupModal);
+// const modal = basicLightbox.create(markupModal);
 
-function openModal(e) {
-    e.preventDefault();
-    modal.show();
-    const body = document.querySelector('body');
-    body.classList.add('bg-scrolling-element-when-modal-open');
-    // const closeModalBtn = document.querySelector('[data-modal-close]');
-    // closeModalBtn.addEventListener('click', closeModal);
-    window.addEventListener('keydown', closeModalHandler);
-    const backdrop = document.querySelector('.basicLightbox');
-    backdrop.addEventListener('click', closeBackdrop);
+// function openModal(e) {
+//     e.preventDefault();
+//     modal.show();
+//     const body = document.querySelector('body');
+//     body.classList.add('bg-scrolling-element-when-modal-open');
+//     // const closeModalBtn = document.querySelector('[data-modal-close]');
+//     // closeModalBtn.addEventListener('click', closeModal);
+//     window.addEventListener('keydown', closeModalHandler);
+//     const backdrop = document.querySelector('.basicLightbox');
+//     backdrop.addEventListener('click', closeBackdrop);
 
-    // function closeModal(e) {
-    //     modal.close();
-    //     body.classList.remove('bg-scrolling-element-when-modal-open');
-    //     closeModalBtn.removeEventListener('click', closeModal);
-    // }
+//     // function closeModal(e) {
+//     //     modal.close();
+//     //     body.classList.remove('bg-scrolling-element-when-modal-open');
+//     //     closeModalBtn.removeEventListener('click', closeModal);
+//     // }
 
-    function closeModalHandler(e) {
-        if (e.key === 'Escape') {
-            modal.close();
-            body.classList.remove('bg-scrolling-element-when-modal-open');
-            window.removeEventListener('keydown', closeModalHandler);
-        }
-    }
+//     function closeModalHandler(e) {
+//         if (e.key === 'Escape') {
+//             modal.close();
+//             body.classList.remove('bg-scrolling-element-when-modal-open');
+//             window.removeEventListener('keydown', closeModalHandler);
+//         }
+//     }
 
-    function closeBackdrop(e) {
-        if (e.target === backdrop) {
-            modal.close();
-            body.classList.remove('bg-scrolling-element-when-modal-open');
-            backdrop.removeEventListener('click', closeBackdrop);
-        }
-    }
-}
+//     function closeBackdrop(e) {
+//         if (e.target === backdrop) {
+//             modal.close();
+//             body.classList.remove('bg-scrolling-element-when-modal-open');
+//             backdrop.removeEventListener('click', closeBackdrop);
+//         }
+//     }
+// }
