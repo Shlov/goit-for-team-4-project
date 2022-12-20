1d79b4d4fa9b59b7a38766112fadab00ca6ef50b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-20 02:21:33 +0200

    Разбил массив со словами в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './js/fetch';
+import './js/langArr';
 import './js/modal';
 import './js/pagination-base';
 import './js/loader';
 import './js/render-top-films';
 import './js/scroll';
-import './js/trailer'
-import './js/switchLanguage'
+import './js/trailer';
+import './js/switchLanguage';
---
+++ src/js/langArr.js
@@ -0,0 +1,72 @@
+export const langArr = {
+  webTitle: {
+    uk: 'Фільмотека',
+    en: 'Filmoteka',
+  },
+
+  headerTitle: {
+    uk: 'Фільмотека',
+    en: 'Filmoteka',
+  },
+  homepage: {
+    uk: 'Домашня сторінка',
+    en: 'Home',
+  },
+  myLibrary: {
+    uk: 'Моя бібліотека',
+    en: 'My library',
+  },
+  movieSearch: {
+    uk: 'Пошук фильмів',
+    en: 'Movie search',
+  },
+  languageChoose: {
+    uk: 'Мова',
+    en: 'Language',
+  },
+  watchBtn: {
+    uk: 'Дивитися',
+    en: 'Watch',
+  },
+  queue: {
+    uk: 'Черга',
+    en: 'queue',
+  },
+  // ? modal window ?
+  vote: {
+    uk: 'Оцiнка / оцiнки',
+    en: 'Vote / votes',
+  },
+  popular: {
+    uk: 'Популярнiсть',
+    en: 'Popularity',
+  },
+  originalTitle: {
+    uk: 'Оригінальна назва',
+    en: 'Original title',
+  },
+  genre: {
+    uk: 'Жанр',
+    en: 'Genres',
+  },
+  about: {
+    uk: 'Про фiльм',
+    en: 'About',
+  },
+  watchedBtn: {
+    uk: 'Додати до переглянутого',
+    en: 'Add to watched',
+  },
+  removeWatchedBtn: {
+    uk: 'Прибрати iз переглянутих',
+    en: 'REMOVE FROM WATCHED',
+  },
+  queueBtn: {
+    uk: 'Додати до черги',
+    en: 'Add to queue',
+  },
+  removeQueueBtn: {
+    uk: 'Прибрати з черги',
+    en: 'Remove from Watched',
+  },
+};
